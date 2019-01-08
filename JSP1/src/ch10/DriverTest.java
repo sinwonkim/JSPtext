7405507f5c99b7d601de20bd7f18845173a4cf7f
sinwonkim <zhfps114@gmail.com>
2019-01-09 07:44:55 +0900

    # JDBC를 통한 MySQL과 연동테스트

---
+++ JSP1/src/ch10/DriverTest.java
@@ -0,0 +1,19 @@
+package ch10;
+
+import java.sql.*;
+public class DriverTest {
+
+	//JDBC를 통한 MySQL과 연동 테스트 
+	public static void main(String[] args) {
+		Connection con;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BBS", "root", "1234");
+			System.out.println("Success");
+		}
+		catch(SQLException ex) {System.out.println("SQLException"+ex);}
+		catch(Exception ex) {System.out.println("Exception:"+ex);}
+	}
+
+}
